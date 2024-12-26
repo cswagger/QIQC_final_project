de234959160e45e003e3d9a5b8dcf84912f14bbf
cswagger <lucas.chensw@gmail.com>
2024-12-26 16:15:53 +0800

    Add 3_SAT_solver.py

--- SAT/3_SAT_solver.py
+++ SAT/3_SAT_solver.py
@@ -1,9 +1,14 @@
-from qiskit_algorithms import Grover
+# import Qiskit quantum libraries
+from qiskit.visualization import plot_histogram
+from qiskit.utils import QuantumInstance
+from qiskit.algorithms import Grover
 from qiskit.circuit.library import PhaseOracle
-from qiskit_aer import Aer
-from qiskit_algorithms import AmplificationProblem
-
+from qiskit import Aer
+from qiskit.algorithms import AmplificationProblem
+import numpy as np
 import os
+import matplotlib.pyplot as plt
+
 with open('3sat3-5.cnf', 'r') as f:
     sat_cnf = f.read()
     oracle = PhaseOracle.from_dimacs_file("3sat3-5.cnf")
@@ -13,4 +18,25 @@ backend = Aer.get_backend('qasm_simulator')
 grover = Grover(quantum_instance=backend)
 problem = AmplificationProblem(oracle)
 result = grover.amplify(problem)
-print(result.assignment)
+histogram = plot_histogram(result.circuit_results)
+print(result.circuit_results)
+
+print("Result assignment:", result.assignment)
+print("Iterations performed:", result.iterations)
+
+results = result.circuit_results[0]
+
+counts = np.array(list(results.values()))
+
+mean = np.mean(counts)
+std_dev = np.std(counts)
+
+threshold = mean + std_dev
+
+high_values = {state: count for state, count in results.items() if count > threshold}
+print("High-value states:", high_values)
+
+
+print(f"Total qubits used: {oracle.num_qubits}")
+oracle.draw(output="mpl")
+plt.show()
